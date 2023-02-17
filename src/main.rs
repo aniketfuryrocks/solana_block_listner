4c8c31d36f8474a57be5fb87a7c35d3d9e9bb4f6
aniketfuryrocks <contact@aniketprajapati.me>
2023-02-17 11:36:43 +0530

    reqwest listner

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,38 @@ use solana_sdk::commitment_config::CommitmentConfig;
 use solana_transaction_status::TransactionDetails;
 
 use self::listner::Listner;
+use self::reqwest_listner::ReqwestListner;
 use cli::Args;
 
 mod cli;
 mod listner;
+mod reqwest_listner;
 
 #[tokio::main]
 pub async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
-    let Args { rpc_addr } = Args::parse();
+    let Args {
+        rpc_addr,
+        use_reqwest,
+    } = Args::parse();
 
-    let listner = Listner {
-        rpc_client: RpcClient::new(rpc_addr),
-    };
+    if use_reqwest {
+        let listner = ReqwestListner {
+            rpc_client: reqwest::ClientBuilder::new().build()?,
+            rpc_addr,
+        };
 
-    listner
-        .listen(CommitmentConfig::finalized(), TransactionDetails::Full)
-        .await?;
+        listner.listen("finalized", "full").await?;
+    } else {
+        let listner = Listner {
+            rpc_client: RpcClient::new(rpc_addr),
+        };
+
+        listner
+            .listen(CommitmentConfig::finalized(), TransactionDetails::Full)
+            .await?;
+    }
 
     bail!("Listener exited")
 }
